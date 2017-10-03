2268e6a82622e02e3f1e0ebb11f11128a15634cb
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-03 16:40:15 -0700

    First commit developing basic structure

---
+++ gradebook.py
@@ -0,0 +1,17 @@
+# gradebook.py
+
+class Classroom():
+
+    """
+    Must be able to display roster for all students as well as GPAs and
+    assignment totals, as well as attendances
+    """
+
+class Student():
+
+    """
+    student must receive a grade (GPA: double) for an assignment (string) and
+    must be able to check whether or not student completed the assignment
+    """
+
+# Perhaps a third class that displays the UI/UX
