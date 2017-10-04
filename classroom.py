831f212a64256b78f91a2761848a5e8df7b3e8c6
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-03 17:51:12 -0700

    Restructuring files

---
+++ classroom.py
@@ -0,0 +1,27 @@
+# gradebook.py
+
+class Classroom(object):
+
+    """
+    Must be able to display roster for all students as well as GPAs and
+    assignment totals, as well as attendances
+    """
+
+    def __init__(self, roster, class_name):
+        # Initialize roster
+        # Initialize class name
+        pass
+
+    """
+    General assignment manager that accepts input from UI class and sends
+    assignment data for sorting
+    """
+    def add_assign(self):
+        pass
+
+    """
+    If a new student enters the class after the professor's initialization, the
+    student's ID will be appended to the end of the name bank and the ID
+    """
+    student_name_bank = ["Aaron Aaronson", "Adonis Creed", "Barry Benson", "Crazy Cathy", "Doug Dimmadome", "Daryl Dixon", "Ernie Everclear"]
+    new_student_counter = 0
