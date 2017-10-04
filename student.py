1ad7ee1c353e2206c39e66888a29f567fd89c8b8
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-03 17:55:29 -0700

    Restructuring files

---
+++ student.py
@@ -0,0 +1,19 @@
+# student.py
+
+
+# Is a student a subclass of a classroom? Is my dad a software class?
+class Student(object):
+
+    def __init__(self, ):
+        # gimme my eyeholes!
+        pass
+
+    """
+    Create subclasses for each individual student ID for students/professors to edit
+    """
+    student_ids = ["AA001", "AC001", "BB001", "CC001", "DD001", "DD002", "EE001"]
+
+# class AA001(Student):
+
+
+# Perhaps a third class that displays the UI/UX
